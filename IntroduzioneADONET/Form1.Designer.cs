bd0d59029f58bca3d3c57db993738fb34d8b60d1
Sebastiano <sebastiano.gaudeano@live.com>
2017-02-21 15:14:27 +0100

    Ancora più carino

--- IntroduzioneADONET/Form1.Designer.cs
+++ IntroduzioneADONET/Form1.Designer.cs
@@ -150,7 +150,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 353);
+            this.ClientSize = new System.Drawing.Size(728, 339);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
