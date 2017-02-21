51a287f8cf18997d9785600e3f29d3a46d8d7cc6
Sebastiano <sebastiano.gaudeano@live.com>
2017-02-21 17:11:37 +0100

    sync

--- IntroduzioneADONET/Form1.Designer.cs
+++ IntroduzioneADONET/Form1.Designer.cs
@@ -117,6 +117,7 @@
             this.button4.TabIndex = 7;
             this.button4.Text = "Save";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // textBox1
             // 
--- IntroduzioneADONET/Form1.cs
+++ IntroduzioneADONET/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace IntroduzioneADONET
 {
@@ -16,5 +17,58 @@ namespace IntroduzioneADONET
         {
             InitializeComponent();
         }
+
+        SqlConnection conn;
+        SqlCommand comm;
+
+        //string connstr = "database=ADONETTutorial;server=GPHP\\SQLEXPR2014;user=sa;password=sapwd";
+
+        string connstr = "DESKTOP-URUIL6H\\SQLEXPRESS;Initial Catalog=AdoTutorial;Integrated Security=True";
+
+        private void button4_Click ( object sender , EventArgs e )
+        {
+            conn = new SqlConnection( connstr );
+            comm = new SqlCommand();
+            conn.Open();
+            // creating instance of SsqlParameter
+            SqlParameter rollno = new SqlParameter( "@rn" , SqlDbType.Int );
+            SqlParameter name = new SqlParameter( "@n" , SqlDbType.VarChar );
+            SqlParameter course = new SqlParameter( "@c" , SqlDbType.VarChar );
+            SqlParameter city = new SqlParameter( "@ci" , SqlDbType.VarChar );
+            // Adding parameter to SqlCommand
+            comm.Parameters.Add( rollno );
+            comm.Parameters.Add( name );
+            comm.Parameters.Add( course );
+            comm.Parameters.Add( city );
+            // Setting values 
+            rollno.Value = Convert.ToInt32( button4.Text );
+            name.Value = button3.Text;
+            course.Value = button2.Text;
+            city.Value = button1.Text;
+            // adding connection to SqlCommand
+            comm.Connection = conn;
+            // Sql Statement
+            comm.CommandText = "insert into studenti values(@rn,@n,@c,@ci)";
+
+            try
+            {
+                comm.ExecuteNonQuery();
+                MessageBox.Show( "Saved" );
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+            }
+            catch ( Exception )
+            {
+                MessageBox.Show( "Not Saved" );
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
     }
 }
+
